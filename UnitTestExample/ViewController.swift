f01ee101094b30807fd2647d7414bb5a6f6346e1
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2020-12-31 22:14:53 +0900

    Implement UI parts.

--- UnitTestExample/ViewController.swift
+++ UnitTestExample/ViewController.swift
@@ -1,12 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextFieldHeaderLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailTextFieldHeaderLabel: UILabel!
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
 
+    }
 }
 
