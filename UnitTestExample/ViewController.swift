885b2f8515884b7732be2070f64766ba76f6d2d7
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2020-12-31 22:15:03 +0900

    Implement business logic.

--- UnitTestExample/ViewController.swift
+++ UnitTestExample/ViewController.swift
@@ -13,6 +13,56 @@ class ViewController: UIViewController {
     }
 
     @IBAction func signupButtonTapped(_ sender: UIButton) {
+        view.endEditing(true)
+        
+        var name: String?
+        if let nameText = nameTextField.text,
+           nameText.count != 0 {
+            name = nameText
+        }
+        
+        var email: String?
+        if let emailText = emailTextField.text,
+           emailText.count != 0 {
+            email = emailText
+        }
+        
+        let userProfile = UserProfile(name: name, email: email)
+        
+        guard userProfile.validate() else {
+            let alertController = UIAlertController(
+                title: "入力エラー",
+                message: "名前を入力してください",
+                preferredStyle: .alert
+            )
+            alertController.addAction(
+                UIAlertAction(
+                    title: "OK",
+                    style: .default,
+                    handler: nil
+                )
+            )
+            present(alertController,
+                    animated: true,
+                    completion: nil)
+            return
+        }
+        
+        let alertController = UIAlertController(
+            title: "登録完了",
+            message: "ありがとうございます",
+            preferredStyle: .alert
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: nil
+            )
+        )
+        present(alertController,
+                animated: true,
+                completion: nil)
 
     }
 }
