d0eef86ccf54358f48fe94abf7dbf6baa12e7b6b
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2020-12-31 21:40:40 +0900

    Initial Commit

---
+++ UnitTestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestExample
+//
+//  Created by hidemune on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
